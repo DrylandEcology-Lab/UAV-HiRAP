9c541710977e10ef3f7fc976cccc927d359a847b
HowcanoeWang <howcanoewang@gmail.com>
2017-08-02 19:40:34 +0800

    8.4.6 登出与测试
    Logout and test loginout function
    fix superlink error in base.html
    lack </a>

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(Form):
-    email = StringField('Email', validators=[DataRequired(), length(1,64),
+    email = StringField('Email', validators=[DataRequired(), Length(1,64),
                                              Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
--- app/main/views.py
+++ app/main/views.py
@@ -9,23 +9,4 @@ from ..models import User
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if current_app.config['MAIL_ADMIN']:
-                send_email(current_app.config['MAIL_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
-            flash('Your name have been sent to administrator!')
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False),
-                           current_time=datetime.utcnow())
+    return render_template('index.html')
